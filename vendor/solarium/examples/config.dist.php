d48701bf6bccb462598ee1478ef717a57204f259
kans-th0 <kans-th0@klth147846.net.lth.se>
2016-05-02 10:44:55 +0200

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- vendor/solarium/examples/config.dist.php
+++ vendor/solarium/examples/config.dist.php
@@ -3,7 +3,7 @@
 $config = array(
     'endpoint' => array(
         'localhost' => array(
-            'host' => '130.235.97.230',
+            'host' => 'benny.net.lth.se',
             'port' => 8983,
             'path' => '/solr/lup/',
             'timeout' => 5000
