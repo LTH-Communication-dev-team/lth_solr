1ae109019bde39e35d916b6c9128e891db49fa2e
kans-th0 <kans-th0@klth147846.net.lth.se>
2016-05-16 15:55:35 +0200

    Merge origin/master
    Conflicts:
    	classes/class.get_menu_functions.php
    	res/get_menu.php

--- class.tx_sampleflex_addFieldsToFlexForm.php
+++ class.tx_sampleflex_addFieldsToFlexForm.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+error_reporting(E_ALL);
 class user_sampleflex_addFieldsToFlexForm {
     
     
